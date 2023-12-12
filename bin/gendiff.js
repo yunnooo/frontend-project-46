811ceb8f96addc5f09e4744c2315be9e52cb53b4
yunnooo <jgr4739@mail.ru>
2023-12-12 13:07:09 +0300

    modified gendiff.js and finished 2step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,6 @@ const program = new Command()
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0', '-V, --version', 'output the version number');
-
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format', 'stylish');
 program.parse(process.argv);
