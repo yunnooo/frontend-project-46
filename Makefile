048f37dc98cfa67da0385acf72cbf4ac62b57257
yunnooo <jgr4739@mail.ru>
2023-12-15 15:57:18 +0300

    finished 3step

--- Makefile
+++ Makefile
@@ -3,5 +3,12 @@ install:
 
 gendiff:
 	node/gendiff.js
+
 publish:
 	npm publish --dry-run
+
+run:
+	node bin/gendiff __fixtures__/file1.json __fixtures__/file2.json
+
+test:
+	gendiff __fixtures__/file1.json __fixtures__/file2.json
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import gendiff from '../src/index.js';
 
 const program = new Command()
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'output format', 'stylish');
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filePath1> <filePath2>')
+  .action((filePath1, filePath2) => {
+    console.log(gendiff(filePath1, filePath2));
+  })
 program.parse(process.argv);
